f74348dd9677d1c2a4f28dae4be7d628d1a87750
mryodo <a.s.savostyanov@gmail.com>
2017-09-27 21:08:02 +0300

    Updated GRAS with GRAS_new

--- comparator.m
+++ comparator.m
@@ -1,11 +1,11 @@
-A0=xlsread('ukrstat2014.xlsx', 'Sheet1', 'B6:T24');
-At=xlsread('ukrstat2015.xlsx', 'Лист1', 'B6:T24');
-u=xlsread('ukrstat2015.xlsx', 'Лист1', 'U6:U24');
-v=xlsread('ukrstat2015.xlsx', 'Лист1', 'B25:T25');
+A0=xlsread('ukrstat2015.xlsx', 'Лист1', 'B6:T24');
+At=xlsread('ukrstat2014.xlsx', 'Sheet1', 'B6:T24');
+u=xlsread('ukrstat2014.xlsx', 'Sheet1', 'U6:U24');
+v=xlsread('ukrstat2014.xlsx', 'Sheet1', 'B25:T25');
 
 A0(isnan(A0))=1e-2;
 At(isnan(At))=1e-2;
-A=MyGRAS_1(A0, u, v);
+A=GRAS(A0, u, v);
 
 mape=MAPE(A, At)
 wape=WAPE(A, At)
